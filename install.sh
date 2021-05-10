d32cc9fe8c52fb8c9544882d9015157a60acf8a0
fastily <fastily@users.noreply.github.com>
2021-05-10 16:58:13 -0700

    handle m1 devices in install script

--- install.sh
+++ install.sh
@@ -2,10 +2,24 @@
 
 cd "${0%/*}" &> /dev/null
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-brew install python
-brew install --cask chromedriver
+if [ $(sysctl -n machdep.cpu.brand_string) == "Apple M1" ]; then # TODO: zsh compatability
+    cat >> ~/.bash_profile <<- 'EOF'
+        for d in "/opt/homebrew/sbin" "/opt/homebrew/bin"; do
+            if [ -d "$d" ]; then
+                    export PATH="${d}:${PATH}"
+            fi
+        done
+EOF
+
+    export "PATH=/opt/homebrew/sbin:/opt/homebrew/bin:${PATH}"
+fi
+
+brew install bash python
+brew install --cask chromedriver google-chrome
 
 pip3 install virtualenv
 virtualenv venv_bought_bot
